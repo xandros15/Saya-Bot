cf1e741dacc41a396d8d777e1e282aeaa99fa3db
xandros15 <xandros92@gmail.com>
2015-12-26 21:32:15 +0100

    better server interface

--- Library/BotInterface/ServerController.php
+++ Library/BotInterface/ServerController.php
@@ -8,24 +8,8 @@ interface ServerController
     public function connect();
 
     public function disconnect();
-    
+
     public function isConnected();
-    
+
     public function loadData();
-
-    public function setName($name);
-
-    public function setHost($address);
-
-    public function setPorts($ports);
-
-    public function getName();
-
-    public function getHost();
-
-    public function getPorts();
-
-    public function getPassword();
-
-    public function getTextline();
 }
