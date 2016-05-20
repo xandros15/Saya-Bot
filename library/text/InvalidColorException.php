8d6ed486f4be2704e0c337d5aa8f5cac16f72433
xandros15 <xandros92@gmail.com>
2016-05-20 13:33:18 +0200

    #3 Create color class with exception class

---
+++ library/text/InvalidColorException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-05-20
+ * Time: 01:34
+ */
+
+namespace library\text;
+
+
+use InvalidArgumentException;
+
+class InvalidColorException extends InvalidArgumentException
+{
+}
