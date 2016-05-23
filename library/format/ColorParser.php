69f4d9dfd41e851183a6f2e8b0b0d6a2c9656651
xandros15 <xandros92@gmail.com>
2016-05-23 18:34:05 +0200

    #3 create color parser

---
+++ library/format/ColorParser.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-05-20
+ * Time: 13:08
+ */
+
+namespace library\format;
+
+
+class ColorParser extends Color
+{
+    const COLOR_REGEX = '/@(\w+)(?:,@(\w+))?{([^{}]+)}/';
+
+    /**
+     * Coloring text by parser
+     * Example input: @ red{this text will be red} and @ blue,@ pink{this text will be blue on ping background}
+     *
+     * @return ColorParser
+     */
+    public function parse() : self
+    {
+
+        $callback = function (array $matches) {
+            list($match, $color, $background, $text) = $matches;
+            try {
+                return $this->getPrefix($color, $background) . $text . $this->getSuffix();
+            } catch (InvalidColorException $exception) {
+                return $match;
+            }
+        };
+
+        $this->formatted = preg_replace_callback(self::COLOR_REGEX, $callback, $this->formatted);
+
+        return $this;
+    }
+}
