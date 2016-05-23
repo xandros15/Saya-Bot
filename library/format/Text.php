ce0d6c608236702a0b97903beee5070fee63bb3b
xandros15 <xandros92@gmail.com>
2016-05-23 18:33:36 +0200

    #3 create basic format functions

---
+++ library/format/Text.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-05-23
+ * Time: 17:05
+ */
+
+namespace library\format;
+
+
+class Text implements Format
+{
+    /** @var  string */
+    protected $formatted;
+
+    /** @var  string */
+    protected $original;
+
+    /**
+     * Format constructor.
+     * @param string $input
+     */
+    public function __construct(string $input)
+    {
+        $this->formatted = $this->original = $input;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString() : string
+    {
+        return $this->formatted;
+    }
+
+    /**
+     * @param string $color
+     * @param string $background
+     * @return Text
+     */
+    public function toColor(string $color, string $background = '') : self
+    {
+        try {
+            $this->formatted = (string) (new Color($this->formatted))->toColor($color, $background);
+        } catch (InvalidColorException $exception) {
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Text
+     */
+    public function toBold() : self
+    {
+        $this->formatted = self::BOLD . $this->formatted . self::BOLD;
+        return $this;
+    }
+
+    /**
+     * @return Text
+     */
+    public function toItalic() : self
+    {
+        $this->formatted = self::ITALIC . $this->formatted . self::ITALIC;
+        return $this;
+    }
+
+    /**
+     * @return Text
+     */
+    public function toUnderline() : self
+    {
+        $this->formatted = self::UNDERLINE . $this->formatted . self::UNDERLINE;
+        return $this;
+    }
+}
